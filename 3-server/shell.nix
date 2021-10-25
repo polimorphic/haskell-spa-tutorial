4451871f5baa6846d5985593a576a39e5bd6c9f6
Daniel Smith <danielspencersmith@gmail.com>
2021-10-25 14:49:32 -0700

    Fix shell files

--- 3-server/shell.nix
+++ 3-server/shell.nix
@@ -1,10 +1,10 @@
 with (import ./nix);
 with pkgs.lib;
 let
-  todo = pkgs.haskell.packages.ghc865.todo;
+  todo = pkgs.haskell.packages.ghc8107.todo;
   deps = todo.getBuildInputs.haskellBuildInputs;
   filteredDeps = filter (x: x != todo) deps;
-  inputs = pkgs.haskell.packages.ghc865.ghcWithHoogle (p: filteredDeps);
+  inputs = pkgs.haskell.packages.ghc8107.ghcWithHoogle (p: filteredDeps);
   shell = pkgs.stdenv.mkDerivation {
     name = "shell";
     buildInputs = with pkgs; [ inputs cabal-install ];
--- 6-ghcjs/shell-ghcjs.nix
+++ 6-ghcjs/shell-ghcjs.nix
@@ -1,10 +1,10 @@
 with (import ./nix);
 with pkgs.lib;
 let
-  todo = pkgs.haskell.packages.ghcjs86.todo;
+  todo = pkgs.haskell.packages.ghcjs810.todo;
   deps = todo.getBuildInputs.haskellBuildInputs;
   filteredDeps = filter (x: x != todo) deps;
-  inputs = pkgs.haskell.packages.ghcjs86.ghcWithHoogle (p: filteredDeps);
+  inputs = pkgs.haskell.packages.ghcjs810.ghcWithPackages (p: filteredDeps);
   shell = pkgs.stdenv.mkDerivation {
     name = "shell";
     buildInputs = with pkgs; [ inputs cabal-install ];
