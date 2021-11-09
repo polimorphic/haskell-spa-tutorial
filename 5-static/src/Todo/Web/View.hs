4e17d15a575b1e827af4e0f26d581f84bcc03743
Georgia Essig <georgia@Georgias-MacBook-Pro.local>
2021-11-09 14:21:14 -0500

    Update static view

--- 5-static/src/Todo/Web/View.hs
+++ 5-static/src/Todo/Web/View.hs
@@ -1,6 +1,10 @@
 module Todo.Web.View (view) where
 
-import Miso (View, div_)
+import Miso (View, div_, button_, class_)
 
 view :: View a
-view = div_ [] ["Todo"]
+view = div_ [] 
+    ["Todo"
+    , div_ [][ button_ [] ["Click me!"]] 
+    , div_ [class_ "red-text"] ["I am Clifford"] 
+    ]
